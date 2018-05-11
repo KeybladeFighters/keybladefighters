5bdbe6cc59d9ca33801afdc21ca6343b2ecf7321
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-05-11 18:31:35 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
 });
-client.on('ready', () => { client.user.setGame('GAME HERE') })
+client.on('ready', () => {client.user.setGame('GAME HERE') });
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
