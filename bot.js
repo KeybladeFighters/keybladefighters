52e99b77beaad30d0050aae178cfb64b868bf7ef
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-05-29 03:16:52 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,7 +71,19 @@ client.on('message', msg => {
   }
 });
 
+client.on('message', async message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
 
+  if (message.content === '/join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      const connection = await message.member.voiceChannel.join();
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
 
 client.on('message', function(message) {
     // Now, you can use the message variable inside
