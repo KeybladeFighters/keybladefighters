8195e676e46efde2e6f562be37e0d38915b77d5b
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-05-29 02:45:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,8 @@ const client = new Discord.Client();
 let prefix = "!";
 
 
-client.on('ready', () => { client.user.setGame('KHUX PvP') });
+client.on('ready', () => { client.user.setGame("xd", "https://www.twitch.tv/monstercat") });
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
