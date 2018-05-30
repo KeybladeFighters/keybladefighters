1c6ce1e419add64924253bce0b5e4a8c4f74f0eb
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-05-30 02:35:39 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,6 +56,20 @@ client.on("ready", () => {
 
 });
 
+exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("You do not have permissions.")
+    let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+    if (!rMember) return message.reply("Please provide a user name")
+    let role = args.join(" ").slice(22);
+
+    if (!role) return message.reply("Please provide a role name.");
+    let aRole = message.guild.roles.find(`name`, role);
+    if (!aRole) return message.reply(`I can't find the role.`);
+
+    if (rMember.roles.has(aRole.id)) return message.reply("The user already have this role!");
+    await (rMember.addRole(aRole.id))
+
+}
 
 client.on("message", function(message) {
     if (message.author.equals(client.user)) return;
