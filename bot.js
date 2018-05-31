d1e4a36dd5fa63701e96520adf72421e473c62f4
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-05-30 21:19:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -124,35 +124,37 @@ client.on("message", function(message) {
             message.channel.send(`MP enviado a ${message.author}`);
             break;
         
+		    
          case "comandos":
       const embed = new Discord.RichEmbed()
-  .setTitle("This is your title, it can hold 256 characters")
-  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+  .setTitle("Comandos")
+  .setAuthor("KeybladeFighters, "https://i.imgur.com/dnyKx2z.jpg")
   /*
    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
    */
   .setColor(0x00AE86)
   .setDescription("This is the main body of text, it can hold 2048 characters.")
-  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-  .setImage("http://i.imgur.com/yVpymuV.png")
-  .setThumbnail("http://i.imgur.com/p2qNFag.png")
+  .setFooter("Se acepta cualquier sugerencia.", "https://i.imgur.com/dnyKx2z.jpg")
+  .setImage("https://i.imgur.com/dnyKx2z.jpg")
+  .setThumbnail("https://i.imgur.com/dnyKx2z.jpg")
   /*
    * Takes a Date object, defaults to current date.
    */
   .setTimestamp()
-  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-  .addField("This is a field title, it can hold 256 characters",
-    "This is a field value, it can hold 2048 characters.")
+  .setURL("https://www.youtube.com/channel/UCfC5uaY01NBD-vIq5tXfaag?view_as=subscriber")
+  .addField("!camii",
+    "Descripcion: Publica el canal de YT de camii.")
   /*
    * Inline fields may not display as inline if the thumbnail and/or image is too big.
    */
-  .addField("Inline Field", "They can also be inline.", true)
+  .addField("!roll", "Descripcion: Publica un numero al azar del 1 al 6", true)
   /*
    * Blank field, useful to create some space.
    */
   .addBlankField(true)
-  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
-
+  .addField("!slap @*<usuario>*", "Descripcion: Golpea a un usario.", true);
+		    
+  .addField("!8ball *<Pregunta>*", "Descripcion: Responde la pregunta que le hagas.", true);
   message.channel.send({embed});
 		    
             }
