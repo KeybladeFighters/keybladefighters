6b093322f8b7e312e8cc975f4e49e0a05e250ff3
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-05-31 02:43:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -86,6 +86,15 @@ client.on("message", function(message) {
                     }});
             }
             break;
+		    case"addrole":
+		    
+		    	role = message.server.roles.get("name", 'Vulpes');
+client.addMemberToRole(user, role, function (err) {
+    if (err) {
+        console.log(err);
+    }
+});
+		    break;
         
           case "roll":
         message.channel.send({embed: {
@@ -165,24 +174,6 @@ client.on("message", function(message) {
 
 
 
-exports.run = async (bot, message, args) => {
-    if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("You do not have permissions.")
-    let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-    if (!rMember) return message.reply("Please provide a user name")
-    let role = args.join(" ").slice(22);
-
-    if (!role) return message.reply("Please provide a role name.");
-    let aRole = message.guild.roles.find(`name`, role);
-    if (!aRole) return message.reply(`I can't find the role.`);
-
-    if (rMember.roles.has(aRole.id)) return message.reply("The user already have this role!");
-    await (rMember.addRole(aRole.id))
-  try{
-   await rMember.send("felicidades ${gRole.name}") 
-  }catch(e){
-    message.channel.send("congrats a <@${rMember.id}>, asignado a ${gRole.name}")
-}
-}
 
 
 
