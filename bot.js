04873a5bd908cb39b2794b89dccdb3146dbb2deb
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-05-31 03:27:55 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -124,13 +124,7 @@ client.on("message", function(message) {
             });
             message.channel.send(`MP enviado a ${message.author}`);
             break;
-		    	        case "punish":
-            let person = message.mentions.members.first();
-            let Punished = message.guild.roles.find(`name`, `Punished`)
-
-            yeet.addRole(Punished).catch(console.error);
-            break;
-
+	
       
 	
         
@@ -176,7 +170,31 @@ client.on("message", function(message) {
 
 
 
-
+exports.run = async (reaction, user) => {
+  console.log('new reacrion')
+  var role
+  var given
+  var msg = await reaction.message
+  if (msg.id == '451647570788352000') {
+    console.log('valid reacrion')
+    if (reaction.emoji.id == '451647570788352000') {
+      role = msg.guild.roles.get('275331552198656000')
+      console.log('new ping reaction')
+    } else if (reaction.emoji.id == '451647570788352000') {
+      role = msg.guild.roles.get('451624985287720962')
+      console.log('new pingo reacrion')
+    }
+    if (user.hasRole(role)) {
+      user.removeRole(role)
+      given = ' You have left @'
+    } else {
+      user.addRole(role)
+      given = ' You have joined @'
+    }
+    console.log('new reacrion done')
+    user.send(reaction.emoji.toString() + given + role.name + '.')
+  }
+}
 
 client.on("message", (message) => {
   
