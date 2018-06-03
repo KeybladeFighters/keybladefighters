b4a516db2e76c1e3ae0c212690d9220f47be091a
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-06-03 01:56:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,7 +57,7 @@ client.on("ready", () => {
 
 });
 
-client.on('messageReactionAdd', (reaction, user, member) => {
+client.on('messageReactionAdd', (reaction, user) => {
 	if(reaction.emoji.name === "431317649780113418")
 	
 	client.channels.get("127500279846273026").send("reaccion ");
