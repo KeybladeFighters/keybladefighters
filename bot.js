21fb8b86a56cca19de86d83210955ce2a97a502b
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-06-02 23:38:38 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,6 +57,42 @@ client.on("ready", () => {
 
 });
 
+
+const events = {
+	MESSAGE_REACTION_ADD: 'messageReactionAdd',
+	MESSAGE_REACTION_REMOVE: 'messageReactionRemove',
+};
+
+client.on('raw', async ({ t: eventName, d: data }) => {
+	if (!events.hasOwnProperty(eventName)) return;
+
+	const channel = client.channels.get(data.channel_id);
+
+	if (channel.messages.has(data.message_id)) return;
+
+	const user = client.users.get(data.user_id);
+	const message = await channel.messages.fetch(data.message_id);
+	const reaction = message.reactions.get(data.emoji.id || data.emoji.name);
+
+	client.emit(events[eventName], reaction, user);
+});
+
+client.on('messageReactionAdd', (reaction, user) => {
+	console.log('Reaction added; current count:', reaction.count);
+});
+
+client.on('messageReactionRemove', (reaction, user) => {
+	console.log('Reaction removed; current count:', reaction.count);
+});
+
+client.on('message', message => {
+	if (message.content === '!react') {
+		console.log('First reaction incoming.');
+		message.react('🤔');
+	}
+});
+
+
 client.on("message", message => {
 
     if(message.content.startsWith(`añadir`)) {
@@ -74,7 +110,7 @@ client.on("message", message => {
 	       const guildMember = message.member;
 
         guildMember.addRole('275331552198656000'); // gets the <GuildMember> from a mention and then adds the role to that member 
-		  guildMember.removeRole('451624985287720962');
+        guildMember.removeRole('451624985287720962');
     }
 });
  client.on("MessageReactionAdd", function(users) {
