336e6f23ee1d95b66454a2853023c11c3e802a12
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-06-06 19:40:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,11 +65,8 @@ var fortunes = [
     "Muy dudoso.",
 ];
 var pelea = [
-    "acaba de abofetear a " ,
-    "golpeó con su keyblade a " ,
-    "ha usado impactrueno a " ,
-    "usó como limpia pisos a " ,
-    "le ha dado una patada en el lóbulo occipital a " ,
+    "acaba de abofetear a   " "file: https://media.giphy.com/media/11VW2xPAb4OFPO/giphy.gif"    ,
+
   
 
 ];
