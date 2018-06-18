13943cbe103602926b3145e91c06c5fc23db34f4
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-06-17 22:32:37 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -313,13 +313,7 @@ client.on("message", function(message) {
             message.reply((pelea[Math.floor(Math.random() * pelea.length)]) + slappedUser);
             break;
 		      
-        case "slop":
-            let slappedUser1 = message.mentions.users.first();
-		      let slappedUser2 = message.mentions.users.second();
-
-            if(message.mentions.users.size < 1) return message.reply("Te olvidaste del nombre a quien pegarle.");
-            message.reply((pelea[Math.floor(Math.random() * pelea.length)]) + slappedUser1 + slappedUser1);
-            break;
+    
 
 
         case "camii":
