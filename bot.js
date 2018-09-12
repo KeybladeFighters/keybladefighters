e8e0807528928ba20f5ffb3fcf97fbb3c1561900
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-09-12 12:06:59 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -801,6 +801,12 @@ client.on("message", msg => {
 		msg.channel.sendMessage(`${msg.author} ha matado a camii!`)
 	}
 	
+	
+	console.log("passive")
+	if (msg.content.startWith("a")) {
+		userData.XP += 10
+	}
+
 		
 
 	console.log(XP)
@@ -824,4 +830,4 @@ client.on('message', function(message) {
 });
 
 
-client.login("");
+client.login("NDQzOTAyNjE0ODgzNzk0OTQ1.DnqvhA.PF82om6mttmxc-xxWEd_JxCOCHA");
