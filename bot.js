4fca9aaca10d3559a789d1157eecd9160e259cdf
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-09-20 22:58:47 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -629,11 +629,11 @@ client.on("message", (message) => {
 
   if(message.content.startsWith (prefix + "serverinfo")){
 
-    let sicon = message.guild.iconURL;
+ 
     let serverembed = new Discord.RichEmbed()
     .setDescription("Server Information")
     .setColor("#15f153")
-    .setThumbnail(sicon)
+    .setThumbnail("https://i.imgur.com/rkfgpxv.jpg")
     .addField("Server Name", message.guild.name)
     .addField("Created On", message.guild.createdAt)
     .addField("You Joined", message.member.joinedAt)
@@ -806,4 +806,4 @@ client.on('message', function(message) {
 
 	
 	
-client.login("");
+client.login("NDQzOTAyNjE0ODgzNzk0OTQ1.DoXKqg.vsDnXXjJQaWXobQcEiwroszGdJA");
