7cd2237f9cae24b60bebe2aee22c5b31caba4909
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2018-09-23 15:47:41 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -543,6 +543,19 @@ client.on("message", (message) => {
   
     
 } else 
+	 if(message.content.startsWith (prefix + "tabla") && message.member.roles.some(r=>["administrador", "moderador", "♔ King ♔ KB Rangers"].includes(r.name)) ) {
+
+        let member = message.mentions.members.first()
+	message.delete(0000);
+		      if(message.mentions.users.size < 1) return message.channel.send("Hya tabla para todos!", {files: ["https://thumbs.gfycat.com/BaggyAppropriateErne-small.gif"]});
+     message.channel.send(` hay tabla para ${member} !`, {
+    file: rando_latigo[Math.floor(Math.random() * rando_latigo.length)]
+	     
+	     
+     });
+  
+    
+} else 
 	 
 
 	 if(message.content.startsWith (prefix + "regaño") && message.member.roles.some(r=>["administrador", "moderador"].includes(r.name)) ) {
@@ -823,4 +836,4 @@ client.on('message', function(message) {
 
 	
 	
-client.login("");
+client.login("NDQzOTAyNjE0ODgzNzk0OTQ1.DolrjQ.f_z_AlNvL_rYs64dcplM8JCzrXY");
